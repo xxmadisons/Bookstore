ce5529340e189e62496abe6186aae58ce92c0be3
xxmadisons <madstanley00@gmail.com>
2022-03-03 13:12:42 -0700

    06-04

--- Models/ViewModels/Buy.cs
+++ Models/ViewModels/Buy.cs
@@ -33,7 +33,5 @@ namespace Bookstore.Models.ViewModels
         [Required(ErrorMessage = "Please enter Country:")]
         public string Country { get; set; }
 
-        public bool Anonymous { get; set; }
-
     }
 }
